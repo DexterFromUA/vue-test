e98113766273cb32a44dcaa2b5608e0dcc2a9e01
Dexter <shalimovvlad@gmail.com>
2019-01-05 20:21:02 +0200

    minor

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <h1 class="text-center display-4 my-5">Super Shop</h1>
     <hr>
     <div class="row">
-      <div class="col-8">
+      <div class="col-7">
         <div class="row">
-          <div class="col-5">
+          <div class="col-5 shadow p-3 mb-5 bg-white rounded">
             <!-- filter block -->
             <div class="form-group">
                 <label for="search">
-                  <h4>Search</h4>
+                  <h4><span class="badge badge-secondary badge-pill">Search</span></h4>
                 </label>
                 <input
                   type="text"
@@ -20,44 +20,64 @@
                 />
             </div>
             <hr>
-            <div>
-              <h4>Price filter</h4>
-              <button class="btn btn-primary mx-5">Dec</button>
-              <button class="btn btn-primary mx-5">Inc</button>
+            <div class="row">
+              <div class="col-6">
+                <h4>Price Sort</h4>
+                <div class="btn-group btn-group-lg" role="group" aria-label="priceSort">
+                  <button @click="priceSort" type="button" class="btn btn-outline-secondary">Dec</button>
+                  <button @click="priceSortReverse" type="button" class="btn btn-outline-secondary">Inc</button>
+                </div>
+              </div>
+              <div class="col-6 text-right">
+                <h4>Title Sort</h4>
+                <div class="btn-group btn-group-lg" role="group" aria-label="titleSort">
+                  <button @click="titleSort" type="button" class="btn btn-outline-secondary">A-Z</button>
+                  <button @click="titleSortReverse" type="button" class="btn btn-outline-secondary">Z-A</button>
+                </div>
+              </div>
             </div>
-            <hr>
-            <div>
-              <h4>Title filter</h4>
-              <button class="btn btn-primary mx-5">A-Z</button>
-              <button class="btn btn-primary mx-5">Z-A</button>
+            <br>
+            <div class="row">
+              <div class="col">
+                <button class="btn btn-outline-warning btn-block">Clear filter</button>
+              </div>
             </div>
           </div>
-          <div class="col">
-            <h4>Items</h4>
+          <div class="col ml-5 shadow p-3 mb-5 bg-white rounded">
+            <h4><span class="badge badge-secondary badge-pill">Items</span></h4>
             <!-- items block -->
             <ul class="list-group">
-              <li class="list-group-item list-group-item-action text-left" v-for="item of filteredItems">{{ item.name }}  <button v-on:click="addTo(item.id, item.name, item.price)" class="btn btn-outline-warning float-right">{{ item.price }}</button></li>
+              <li class="list-group-item list-group-item-action text-left" v-for="item of filtered"><span class="">{{ item.name }}</span><button v-on:click="addTo(item.id, item.name, item.price)" class="btn btn-outline-warning float-right">Add to cart {{ item.price }}</button></li>
             </ul>
           </div>
         </div>
-        <div class="row mt-5">
+        <div class="row">
           <div class="col">
-            <h4>Stats</h4>
+            <h4><span class="badge badge-secondary badge-pill">Stats</span></h4>
+            <div class="row">
+              <div class="col">
+                <p>Total count of orders in the shop: {{ stats.totalItems }}</p>
+              </div>
+              <div class="col">
+                <p>Total count of items in the shop: {{ stats.totalOrders }}</p>
+              </div>
+            </div>
             <!-- stats block -->
-            <p>Total count of orders in the shop: {{ stats.totalItems }}</p>
-            <p>Total count of items in the shop: {{ stats.totalOrders }}</p>
+            
+            
           </div>
         </div>
       </div>
-      <div class="col-4">
+      <div class="col ml-5 shadow p-3 mb-5 bg-white rounded" style="height: 385px">
         <!-- basket block -->
-        <h4>Basket</h4>
+        <h4><span class="badge badge-secondary badge-pill">Cart</span></h4>
         <table class="table">
           <thead>
             <tr>
               <th scope="col">Title</th>
               <th scope="col">Count</th>
               <th scope="col">Price</th>
+              <th scope="col">X</th>
             </tr>
           </thead>
           <tbody>
@@ -79,18 +99,23 @@
                 </button>
               </th>
               <th>{{ item.price }}</th>
+              <th><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></th>
             </tr>
           </tbody>
         </table>
         <hr>
-        <h3 class="text-right">Total: {{ sum }}</h3><br>
-        <button
+        <div class="row">
+          <div class="col-8"><button
           @click="cleanList"
           class="btn btn-outline-danger btn-sm"
         >
           Clean up
-        </button>
-        <button class="btn btn-success btn-lg float-right">Buy selected items</button>
+        </button></div>
+          <div class="col-4">
+            <h3 class="text-right">Total: {{ sum }}</h3><hr>
+          </div>
+        </div>
+        <button class="btn btn-success btn-lg float-right btn-block">Buy selected items</button>
       </div>
     </div>
   </div>
@@ -108,12 +133,7 @@ export default {
       sum: 0,
       currentPrice: 0,
       search: '',
-      filter: {
-        az: false,
-        za: false,
-        more: false,
-        less: false
-      }
+      filtered: []
     };
   },
   mounted() {
@@ -199,12 +219,38 @@ export default {
           item.price = item.count * this.currentPrice;
         }
       })
+    },
+    priceSort() {
+      const comparePrice = (a, b) => (a.price - b.price);
+      return this.filtered = this.filteredItems.sort(comparePrice);
+    },
+    priceSortReverse() {
+      return this.filtered = this.priceSort.reverse()
+    },
+    titleSort() {
+      const compareTitle = (a, b) => {
+        let titleA = a.name.toLowerCase();
+        let titleB = b.name.toLowerCase();
+
+        if(titleA < titleB) return -1
+        if(titleA > titleA) return 1
+        return 0
+      }
+
+      return this.filtered = this.filteredItems.sort(compareTitle);
+    },
+    titleSortReverse() {
+      const f = this.titleSort.reverse();
+      return this.filtered = f;
+    },
+    cleraFilter() {
+      
     }
   },
   computed: {
     filteredItems() {
       return this.items.filter(item => {
-        return item.name.indexOf(this.search) > -1
+        return this.filtered = item.name.indexOf(this.search) > -1
       })
     }
   }
